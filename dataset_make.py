c2f9cd9c0bb31ccfa098d5ece3db340f8b4dbe93
Nate Ruppert <nathaniel.ruppert@sce.com>
2021-05-27 13:09:18 -0700

    added logic to pull dataset info and have variable length temperature assignment per dataset classification

--- dataset_make.py
+++ dataset_make.py
@@ -13,10 +13,24 @@ if __name__ == '__main__':
     # Read in datasets to make
     config = configparser.ConfigParser()
     config.read('data_sets.ini')
-    full = config['Full Voltage']['voltages'].split(', ')
-    shade = config['Shade Voltage']['voltages'].split(', ')
-    data_name = config['Names']['name'].split(', ')
 
+    data_sets = [[key, config[key]['temps']] for key in config.keys() if key != 'DEFAULT']
+    print(data_sets)
+
+    for set, temps in data_sets:
+        if not temps:
+            temp_sets = [27, 30, 35, 40, 45, 50]
+            print(set, temp_sets)
+        else:
+            var_sets = temps.split(', ')
+            temp_sets = []
+            for val in var_sets:
+                if '-' in val:
+                    lower, upper = val.split('-')
+                    results = [x for x in range(int(lower), int(upper)+1)]
+                else: results = [int(val)]
+                temp_sets += results
+            print(set, temp_sets)
 
     for dir in os.scandir(path):
         if dir.is_dir() and dir.name not in data_name:  # ensure directory not file
@@ -25,10 +39,9 @@ if __name__ == '__main__':
                 if sub_dir.is_dir():  # ensure directory not file
                     print(f'Sub Directory: {dir.name}/{sub_dir.name}')
                     for Full_Voltage, Shade_Voltage, name in list(zip(full, shade, data_name)):
-
                         # Creates the dataset path
                         full_path = os.path.abspath(
-                            f"Spice Simulation\\{name}\\{dir.name}\\{sub_dir.name}\\" )
+                            f"Simulation_Sets\\{name}\\{dir.name}\\{sub_dir.name}\\")
                         os.makedirs(full_path, exist_ok=True)
 
                         # Iterate through files and add them to dataset
@@ -39,13 +52,10 @@ if __name__ == '__main__':
                                     ltspice_file = f.readlines()
 
                                     for line in range(len(ltspice_file)):
-                                        #Regex for search: (?<=[0-9]{4}  0 dc )\d+
+                                        # Regex for search: (?<=[0-9]{4}  0 dc )\d+
                                         ltspice_file[line] = ltspice_file[line].replace('900', Shade_Voltage)
                                         ltspice_file[line] = ltspice_file[line].replace('1000', Full_Voltage)
 
-                                    with open(full_path+'\\'+file.name, 'w') as f:
+                                    with open(full_path + '\\' + file.name, 'w') as f:
                                         f.write(''.join(ltspice_file))
                                     pass
-
-
-
